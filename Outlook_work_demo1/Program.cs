850a4a8cf257350f344b72ada245731f77b828e2
Max <maxim090491@gmail.com>
2020-10-05 16:55:45 +0300

    Последние изменения - вечный цикл обработки с задержкой в минуту

--- Outlook_work_demo1/Program.cs
+++ Outlook_work_demo1/Program.cs
@@ -12,27 +12,7 @@ namespace Outlook_work_demo1
 {
     class Program
     {
-        class FileM 
-        {
-            public static void MoveReplaceFile(string sourceFileName, string destFileName) // метод одназначного переноса файла
-            {
-                
-                    //first, delete target file if exists, as File.Move() does not support overwrite
-                    if (File.Exists(destFileName))
-                    {
-                        File.Delete(destFileName);// удаляем из папки куда копируется файл 
-                    }
-
-                    File.Move(sourceFileName, destFileName); // переместили
-                if (File.Exists(destFileName))// если файл переместился то проверяем адплился ли он из основной папки
-                {
-                    if (File.Exists(sourceFileName))
-                    {
-                        File.Delete(sourceFileName);
-                    }
-                }
-            }
-        }
+        
         static void Main(string[] args)
         {
             //Outlook.Application application; //
@@ -42,8 +22,8 @@ namespace Outlook_work_demo1
             //Microsoft.Office.Interop.Outlook._Folders oFolders;
             ///////////////1 модуль отправки файлов///////////////////
             string[] dirs;
-            dirs = Directory.GetFiles("C://OUT//"); //дирректория - откуда берём файл
-            String dirs1 = Directory.GetFiles("C://OUT//").Length.ToString(); //если в папке out что то есть то отправляем письмо
+            dirs = Directory.GetFiles(@"C:\tkM\OUT"); //дирректория - откуда берём файл
+            String dirs1 = Directory.GetFiles(@"C:\tkM\OUT").Length.ToString(); //если в папке out что то есть то отправляем письмо
 
             for (int i = 0; i <= dirs.Length + 1; i++)//для отправки 1 письмо + 1 файл
             {
@@ -60,9 +40,9 @@ namespace Outlook_work_demo1
                         Outlook._Application _app = new Outlook.Application();
                         Outlook.MailItem mail = (Outlook.MailItem)_app.CreateItem(Outlook.OlItemType.olMailItem);
                         mail.To = "maxim090491@outlook.com"; //"maximusvictor@mail.ru";
-                        mail.Subject = "test1567";
-                        mail.Body = "This is test message1156711";
-                        mail.Importance = Outlook.OlImportance.olImportanceNormal;
+                        mail.Subject = "test1567"; // тема  
+                        mail.Body = "This is test message1156711"; //текст письма
+                        mail.Importance = Outlook.OlImportance.olImportanceNormal; //какае то нужная вещь
 
 
                         foreach (string s in dirs)
@@ -74,7 +54,7 @@ namespace Outlook_work_demo1
 
                                 Console.WriteLine(s);
                                 mail.Attachments.Add(s); //добавляем вложения в письмо - возможно добавить сразу все вложения
-                                File.Move(s, s.Replace("OUT", "ARCH")); //После успешного прикрепления файлой, переносим их в папку C:\ARCH
+                                File.Move(s, s.Replace("OUT", "OUTaRCH")); //После успешного прикрепления файла, переносим их в папку C:\ARCH
                                 ((Outlook._MailItem)mail).Send();//отправляем письмо
                                 Console.WriteLine("Файл отправлен в ТК");// если все хорошо, то выдаём в консоль сообщение
                                 break;// выхожу из цикла, что бы реализовать фичу 1 фал = 1 письмо, и так сойдёт :) рефакторинг потом
@@ -83,20 +63,15 @@ namespace Outlook_work_demo1
                             {
 
                                 Console.WriteLine(ex.Message);
-                                Console.WriteLine("С таким именем файла уже отправлялось\n" + "Перемещаю файл в папку BadPack");
-                                FileM.MoveReplaceFile(s, s.Replace("OUT", "BadPack"));
-                                System.Threading.Thread.Sleep(1000);
+                                Console.WriteLine("С таким именем файла уже отправлялось\n" + "Перемещаю файл в папку Bad");
+                                FileM.MoveReplaceFile(s, s.Replace("OUT", "Bad"));//прописываю каталог C:\tkM\Bad так быстрее не люблю много переменных
+                                System.Threading.Thread.Sleep(1000); //спим 1000 мс что б увидеть работу кода
                                 Console.ReadKey();
 
                             }
 
 
                         }
-
-
-
-
-
                         _app = null; //убиваем ссылки на экземпляр класса
                         mail = null;
 
@@ -105,8 +80,8 @@ namespace Outlook_work_demo1
                     }
                     catch
                     {
-                        Console.WriteLine("Что то не так блять"); // если какае то бага то пишем это сообщение
-                        Console.WriteLine("Переходим к приёму файлов нажми любую клавишу");
+                        Console.WriteLine("Что то пошло не так"); // если какае то бага то пишем это сообщение
+                        Console.WriteLine("Не удалось отправить файлы, переходим к приёму файлов нажми любую клавишу");
                         System.Threading.Thread.Sleep(1000);
                         goto importStart; //если модуль отправки не отработал то программа переходит к модулю чтения сообщений
 
@@ -115,7 +90,7 @@ namespace Outlook_work_demo1
                 }
                 else { Console.WriteLine("Файлов для отправки нет"); break; } //если нет файлов во вложении то выходим из цикла
                 Console.ReadKey();
-                dirs = Directory.GetFiles("C://OUT//"); //присваивание переменной с каждой итерацией цикла для обновления данных в массиве
+                dirs = Directory.GetFiles(@"C:\tkM\OUT"); //присваивание переменной с каждой итерацией цикла для обновления данных в массиве
                 System.Threading.Thread.Sleep(1000);
             }
             importStart:
@@ -151,10 +126,10 @@ namespace Outlook_work_demo1
                 }
 
 
-                Outlook.Items unreadItems = processedFolder.Items.Restrict("[Unread]=true");// работаем с данной папкой и ищем не прочитанные файлы
-                Console.WriteLine("Непрочитанных писем = " + unreadItems.Count.ToString()); // выдать в консоль колличество непрочитанных сообщений
+                Outlook.Items unreadItems = processedFolder.Items.Restrict("[Unread]=true");// работаем с данной папкой и ищем не прочитанные письма
+                Console.WriteLine("Непрочитанных писем в папке in = " + unreadItems.Count.ToString()); // выдать в консоль колличество непрочитанных сообщений
 
-                //////////здесь вываодим в консоль все данные письма можно заккоментить/////////////////////////
+                //////////здесь вываодим в консоль все данные непрочитанные письма в папке in    можно заккоментить/////////////////////////
                 StringBuilder str = new StringBuilder(); //читаем текст и данные письма выыодим в консоль не обязательно, но можно оставить для логирования
                 try
                 {
@@ -209,27 +184,49 @@ namespace Outlook_work_demo1
                     foreach (object collectionItem in inBoxItems)
                     {
                         newEmail = collectionItem as Outlook.MailItem;
-                        DateTime sysdate_ = DateTime.Now; //
+                        DateTime sysdate_ = DateTime.Now; //дата текущий день
                     //mailItem.ReceivedTime.Date.ToShortDateString() + sysdate_.ToShortDateString();
-                    if (newEmail != null && newEmail.ReceivedTime.Date.ToShortDateString() == sysdate_.ToShortDateString())
+                    if (newEmail != null && newEmail.ReceivedTime.Date.ToShortDateString() == sysdate_.ToShortDateString())//проарка на дату файла
                         {
-                        if (newEmail.Attachments.Count > 0)
+                            if (newEmail.Attachments.Count > 0)
                             {
                             for (int i = 1; i <= newEmail.Attachments.Count; i++)
                                 {
-                                string[] dirsIN = Directory.GetFiles("C://OUT//");//создадим перепенную с именами файлов в папке IN или IN_ARCH
-                                //if ()
-                                //    {
-                                        newEmail.Attachments[i].SaveAsFile(@"C:\IN\" + newEmail.Attachments[i].FileName);
-                                        Console.WriteLine(@"Файл сохранён с названием: " + newEmail.Attachments[i].FileName + " По пути:" + @"C:\IN\"); // выводим инфу об успешно копировании файла
+                                string fileName = newEmail.Attachments[i].FileName; Console.WriteLine(@"Имя файла:" + fileName);
+                                string[] dirsINaRCH = Directory.GetFiles(@"C:\tkM\INaRCH\");//создадим перепенную с именами файлов в папке IN или IN_ARCH
+                                if (FileM.ValueOf(dirsINaRCH, fileName) != "noDuble")
+                                {
+                                    Console.WriteLine(@"Найдено совпаление  с файлом: " + fileName + "\n Список файлов в папке:");
+                                    
+                                    for (int i1 = 0; i1 < dirsINaRCH.Length; i1++)
+                                    {
+                                        Console.Write( dirsINaRCH[i1] + " " + File.GetCreationTime(dirsINaRCH[i1]) + ";\n");
+                                    }
 
-                                        if (newEmail.UnRead) // пометить письмо как прочитанное
+                                }
+                                else 
+                                {
+                                    Console.WriteLine(@"нет совпадения файлов");
+                                    //Console.WriteLine(@"Имя файла:" + fileName);
+                                    //for (int i1 = 0; i1 < dirsINaRCH.Length; i1++)
+                                    //{
+                                    //    Console.Write("Имя файлов(а) в папке:" + dirsINaRCH[i1]);
+                                    //}
+                                    newEmail.Attachments[i].SaveAsFile(@"C:\tkM\IN\" + newEmail.Attachments[i].FileName);
+                                    Console.WriteLine(@"Файл сохранён с названием: " + newEmail.Attachments[i].FileName + " По пути:" + @"C:\tkM\IN\"); // выводим инфу об успешно копировании файла
+                                }
+
+                                //    {
+                                Console.WriteLine(@"Обрабатываю письмо как прочитанное");
+                                System.Threading.Thread.Sleep(1000);
+                                if (newEmail.UnRead) // пометить письмо как прочитанное
                                         {
                                             newEmail.UnRead = false;
                                             newEmail.Save();
                                         }
-                                    //}
-                                }
+                                Console.WriteLine(@"Завершаю работу");
+                                //}
+                            }
                             }
                         
                         }
@@ -256,8 +253,41 @@ namespace Outlook_work_demo1
                 oApp = null;
                 //Console.ReadKey(); //Удалить все чтения ввода с консоли, для автоматической работы
             System.Threading.Thread.Sleep(1000);
-
+            
         }
+            class FileM
+            {
+                public static void MoveReplaceFile(string sourceFileName, string destFileName) // метод одназначного переноса файла
+                {
+
+                    //first, delete target file if exists, as File.Move() does not support overwrite
+                    if (File.Exists(destFileName))
+                    {
+                        File.Delete(destFileName);// удаляем из папки куда копируется файл 
+                    }
+
+                    File.Move(sourceFileName, destFileName); // переместили
+                    if (File.Exists(destFileName))// если файл переместился то проверяем адплился ли он из основной папки
+                    {
+                        if (File.Exists(sourceFileName))
+                        {
+                            File.Delete(sourceFileName);
+                        }
+                    }
+                }
+
+                public static string ValueOf(string[] arrayS, string value ) //поиск названия в массиве
+            {
+                for(int i = 0; i < arrayS.Length; i++)
+                {
+                    if (Path.GetFileName(arrayS[i]) == value) //Класс и медод Path.GetFileName  вытаскивает из пути имя файла
+                    {
+                        return value;
+                    }
+                }
+                return "noDuble";
+            }
+            }
         }
     }
 
